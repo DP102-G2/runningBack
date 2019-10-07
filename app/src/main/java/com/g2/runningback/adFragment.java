9af1e955832401d0c5b73678fc556c6491e21d4d
fanfan <a10033135>
2019-10-07 12:08:43 +0800

    CREATE LOGIN ITENT

--- app/src/main/java/com/g2/runningback/adFragment.java
+++ app/src/main/java/com/g2/runningback/adFragment.java
@@ -1,13 +1,18 @@
 package com.g2.runningback;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -16,16 +21,35 @@ import android.view.ViewGroup;
 public class adFragment extends Fragment {
 
 
-    public adFragment() {
-        // Required empty public constructor
-    }
+    Button btLgoion;
+    Intent intent;
+    Activity activity;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = getActivity();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ad, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btLgoion = view.findViewById(R.id.btLogin);
+        btLgoion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(activity, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
